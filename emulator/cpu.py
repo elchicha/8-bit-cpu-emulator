9113e6884067f2beb29078988a95675199206e64
elchicha <github.com.bulginess438@passmail.com>
2026-01-01 11:40:37 -0800

    Implement basic 8-bit register with overflow handling

---
+++ emulator/cpu.py
@@ -0,0 +1,13 @@
+class Register:
+    """An 8-bit register for CPU operations"""
+
+    def __init__(self):
+        self._value = 0
+
+    def get(self):
+        """Get the current value of the register"""
+        return self._value
+
+    def set(self, value):
+        """Set the value of the register to *value*"""
+        self._value = value & 0xFF # Mask to ensure 8-bit value
---
+++ tests/test_register.py
@@ -0,0 +1,23 @@
+from emulator.cpu import Register
+
+class TestRegister:
+    def test_register_initializes_to_zero(self):
+        """A new register should start with value 0"""
+        register = Register()
+        assert register.get() == 0
+
+    def test_register_can_store_value(self):
+        """Register should store an 8-bit value"""
+        register = Register()
+        register.set(0x2C)
+        assert register.get() == 0x2C
+
+
+    def test_register_wraps_on_overflow(self):
+        """Values over 255 should wrap around to 0 (8-bit behavior)"""
+        register = Register()
+        register.set(0x100)
+        assert register.get() == 0
+
+        register.set(0x101)
+        assert register.get() == 1
