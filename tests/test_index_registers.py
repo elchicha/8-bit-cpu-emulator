7cba6081c7100409e9383163e29d8785ebc34099
elchicha <github.com.bulginess438@passmail.com>
2026-01-03 10:46:17 -0800

    test: add ldx and ldy immediate

--- tests/test_index_registers.py
+++ tests/test_index_registers.py
@@ -32,3 +32,35 @@ def test_ldy_immediate():
 
     assert cpu.y_register.get() == 0x33
     assert cpu.program_counter.get() == 0x02
+
+
+def test_stx_absolute():
+    """STX $0200 - Store X register to memory"""
+    cpu = CPU()
+    cpu.x_register.set(0x99)
+
+    # Opcode: 0x8E = STX absolute
+    cpu.memory.write_byte(0x0000, 0x8E)
+    cpu.memory.write_byte(0x0001, 0x00)
+    cpu.memory.write_byte(0x0002, 0x02)
+
+    cpu.step()
+
+    assert cpu.memory.read_byte(0x0200) == 0x99
+    assert cpu.program_counter.get() == 0x03
+
+
+def test_sty_absolute():
+    """STY $0300 - Store Y register to memory"""
+    cpu = CPU()
+    cpu.y_register.set(0x77)
+
+    # Opcode: 0x8C = STY absolute
+    cpu.memory.write_byte(0x0000, 0x8C)
+    cpu.memory.write_byte(0x0001, 0x00)
+    cpu.memory.write_byte(0x0002, 0x03)
+
+    cpu.step()
+
+    assert cpu.memory.read_byte(0x0300) == 0x77
+    assert cpu.program_counter.get() == 0x03
